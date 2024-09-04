cf90d8166eb99a7e1f8198acb738569fc3bfdb68
gijsvandenbeuken <g.vandenbeuken@student.fontys.nl>
2024-09-04 15:29:25 +0200

    improved CSS and interface structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import { Document, HeadingLevel, Packer, Paragraph, TextRun } from "docx";
 import { saveAs } from "file-saver";
 import logo from "/logo.png";
 import "./App.css";
 
 function App() {
-  const [files, setFiles] = useState<File[]>([]);
-  const [uploadStatus, setUploadStatus] = useState<string | null>(null);
-  const [fullPrompt, setFullPrompt] = useState<string | null>(null);
+  const [companyName, setCompanyName] = useState<string | null>(null);
+  const [campaignRapport, setCampaignRapport] = useState<File[]>([]);
+  const [adRapports, setAdRapports] = useState<File[]>([]);
+  const [fileName, setFileName] = useState<string | null>(null);
+  const [isUploading, setIsUploading] = useState<boolean>(false);
   const [response, setResponse] = useState("");
 
-  const [isFinished, setIsFinished] = useState<boolean>(false);
+  /*const introductionParagraph: string = `Bij deze sturen wij je de maandelijkse rapportage van de Google Ads optimalisatie
+  van de lopende campagne. Het is van belang om eerst enkele statistieken in kaart te
+  brengen die ons een idee geven van hoe de campagnes draaien.`;*/
 
-  const [companyName, setCompanyName] = useState<string | null>(null);
+  const prompt: string = `Geef een analyse over het bedrijf "${companyName}". Deel deze op in de volgende paragrafen; "Inleiding" en "Statistieken". Zorg ervoor dat je het totaal aantal clicks, CTR, kosten, CPC, het aantal conversies en de kosten per conversie van de campagnes bij elkaar opgeteld in de "Statistieken" paragraaf duidelijk onder elkaar opbreekt.`;
+
+  const campaignPrompt: string = `Geef een analyze over "${fileName}" van het bedrijf "${companyName}". Geef eerst de naam van de campagne, en deel daarna de analyze op in de paragrafen "Leeftijden", "Geslacht", "Apparaten", "Dag en tijd" en "Doelgroepen". Dit doe je daarna ook met de andere campagnes. Wanneer je iets niet weet of ergens geen (duidelijke) informatie over hebt, geef je dit ook aan bij de betreffende paragraaf. `;
 
   const handleCompanyName = (event: any) => {
     setCompanyName(event.target.value);
   };
 
-  const prompt: string = `Geef een analyse over het bedrijf "${companyName}". Deel deze op in de volgende paragrafen; "Inleiding" en "Statistieken". Zorg ervoor dat je het totaal aantal clicks, CTR, kosten, CPC, het aantal conversies en de kosten per conversie van de campagnes bij elkaar opgeteld in de "Statistieken" paragraaf duidelijk onder elkaar opbreekt. Daarna ga je de individuele campagnes in diepte analyzeren. Je begint bij de eerste campagne, geeft de naam van de campagne aan, en deelt het daarna op in de paragrafen "Leeftijden", "Geslacht", "Apparaten", "Dag en tijd" en "Doelgroepen". Dit doe je daarna ook met de andere campagnes. Wanneer je iets niet weet of ergens geen (duidelijke) informatie over hebt, geef je dit ook aan bij de betreffende paragraaf. `;
-
   const generateWordFile = (text: string) => {
-    // Verdeel de tekst in paragrafen, splitst op dubbele nieuwe regel
     const paragraphs = text.split("\n\n");
 
     const doc = new Document({
@@ -31,14 +33,14 @@ function App() {
           properties: {},
           children: [
             new Paragraph({
-              text: "Google Ads Optimalisatie", // Voeg hier de titel toe
-              heading: HeadingLevel.TITLE, // Optioneel: Pas de titel aan
+              text: "Google Ads Optimalisatie",
+              heading: HeadingLevel.TITLE,
             }),
             ...paragraphs.flatMap((paragraph) => [
               new Paragraph({
                 children: [new TextRun(paragraph)],
               }),
-              new Paragraph({ text: "" }), // Voeg een lege paragraaf toe voor extra ruimte
+              new Paragraph({ text: "" }),
             ]),
           ],
         },
@@ -51,46 +53,46 @@ function App() {
   };
 
   async function handleUpload() {
-    if (files.length === 0) {
-      setUploadStatus("No files selected");
+    if (campaignRapport.length === 0) {
       return;
     }
 
-    const formData = new FormData();
-    files.forEach((file) => formData.append("files", file));
-
-    setUploadStatus("Uploading...");
+    setIsUploading(true);
 
     try {
-      // Stuur de bestanden naar de server (of verwerk ze op een andere manier)
-      const res = await axios.post("http://httpbin.org/post", formData, {
-        headers: {
-          "Custom-Header": "Value",
-        },
-      });
-      console.log(res.data.files);
+      const combineTextAndAnalyze = async (files: File[], prompt: string) => {
+        let combinedText = "";
+        for (const file of files) {
+          combinedText += await file.text();
+        }
 
-      // Combineer de tekst van alle bestanden
-      let combinedText = "";
-      for (const file of files) {
-        combinedText += await file.text();
-      }
-      setUploadStatus("Connecting to server...");
+        const response = await fetch("http://localhost:3001/chat", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ message: prompt + combinedText }),
+        });
 
-      const response = await fetch("http://localhost:3001/chat", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ message: prompt + combinedText }),
-      });
-      const data = await response.json();
-      setResponse(data.content);
-      setUploadStatus("Response retrieved successfully!");
-      setIsFinished(true);
-      setFullPrompt(prompt + combinedText);
+        return await response.json();
+      };
+
+      const analyzedCampagne = await combineTextAndAnalyze(
+        campaignRapport,
+        prompt
+      );
+      setFileName(campaignRapport.map((file) => file.name).join(", ")); // indien nodig
+
+      const analyzedAdRapport = await combineTextAndAnalyze(
+        adRapports,
+        campaignPrompt
+      );
+      setFileName(adRapports.map((file) => file.name).join(", ")); // indien nodig
+
+      setResponse(analyzedCampagne.content + analyzedAdRapport.content);
+      setIsUploading(false);
     } catch (err) {
-      setUploadStatus("Upload failed" + err);
+      setIsUploading(false);
       console.log(err);
     }
   }
@@ -104,34 +106,40 @@ function App() {
   return (
     <div className="container">
       <div>
-        <a href="https://www.google.com/" target="_blank">
-          <img src={logo} className="logo" alt="Geen Gedoe Google Ads logo" />
-        </a>
+        <img src={logo} className="logo" alt="Geen Gedoe Google Ads logo" />
       </div>
-      <label>Datasets</label>
-      <input
-        type="file"
-        multiple
-        onChange={(e) => {
-          if (e.target.files) {
-            setFiles(Array.from(e.target.files));
-          }
-        }}
-      />
-      <label>Bedrijfsnaam</label>
-      <input onChange={handleCompanyName}></input>
-      <button onClick={handleUpload}>Upload</button>
-      {uploadStatus && (
-        <span className={isFinished ? "finished" : "notFinished"}>
-          {uploadStatus}
-        </span>
-      )}
-      {fullPrompt && (
-        <>
-          <h3>Question asked:</h3>
-          <pre>{fullPrompt}</pre>
-        </>
-      )}
+      <div className="dataForm">
+        <label>Bedrijfsnaam</label>
+        <input onChange={handleCompanyName}></input>
+        <label>Campagnerapport</label>
+        <input
+          className="fileInput"
+          type="file"
+          multiple
+          onChange={(e) => {
+            if (e.target.files) {
+              setCampaignRapport(Array.from(e.target.files));
+            }
+          }}
+        />
+        <label>Advertentierapporten</label>
+        <input
+          className="fileInput"
+          type="file"
+          multiple
+          onChange={(e) => {
+            if (e.target.files) {
+              setAdRapports(Array.from(e.target.files));
+            }
+          }}
+        />
+      </div>
+      <button onClick={handleUpload} disabled={isUploading}>
+        {isUploading === true
+          ? "Verwerken, even geduld..."
+          : "Laat ChatGPT analyzeren"}
+      </button>
+      <small>versie: 1.0.0, model: OpenAI ChatGPT-4o</small>
     </div>
   );
 }
