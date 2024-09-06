8a3d211dca160763a41aa5054ebfa5ec225a8e8d
gijsvandenbeuken <g.vandenbeuken@student.fontys.nl>
2024-09-06 14:32:39 +0200

    made the code compact and usable for retrieved Google Ads Scripts data

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,16 @@ SVGAnimateTransformElement;
 
 function App() {
   const [companyName, setCompanyName] = useState<string | null>(null);
-  const [campaignRapport, setCampaignRapport] = useState<File[]>([]);
-  // const [campaignDataFiles, setCampaignDataFiles] = useState<File[]>([]);
+  const [campaignData, setCampaignData] = useState<File[]>([]);
   const [isUploading, setIsUploading] = useState<boolean>(false);
   const [response, setResponse] = useState("");
-
-  const [campaignNames, setCampaignNames] = useState<string[]>(["", "", ""]);
-  const [campaignNamesCounter, setCampaignNamesCounter] = useState<number>(0);
-  const [campaignOneDataFiles, setCampaignOneDataFiles] = useState<File[]>([]);
-  const [campaignTwoDataFiles, setCampaignTwoDataFiles] = useState<File[]>([]);
-  const [campaignThreeDataFiles, setCampaignThreeDataFiles] = useState<File[]>(
-    []
-  );
-  //const [campaginData, setCampaignData] = useState<string[]>(["", "", ""]);
-
   const [currentDate] = useState<string>(
     new Date().toISOString().split("T")[0]
   );
 
-  const prompt: string = `Geef eerst onder elkaar met een regel witruimte het aantal Clicks de CTR, de Kosten, de CPC, het Aantal Conversies en de Kosten per Conversie. Schrijf daarna één paragraaf over de statistieken van alle campagnes bij elkaar opgeteld. zet GEEN titel boven de paragraaf, ik wil enkel de inhoudelijke paragraaftekst.`;
+  const overviewPrompt: string = `Geef eerst onder elkaar met een regel witruimte het aantal Clicks de CTR, de Kosten, de CPC, het Aantal Conversies en de Kosten per Conversie. Schrijf daarna één paragraaf over de statistieken van alle campagnes bij elkaar opgeteld. zet GEEN titel boven de paragraaf, ik wil enkel de inhoudelijke paragraaftekst.`;
 
-  /*
-  const campaignPrompt: string = `Geef een analyze over de campagnes van het bedrijf "${companyName}". Start telkens met de naam van de campagne, en werk daarna de analyze uit in 5 alinea's; "Leeftijden", "Geslacht", "Apparaten", "Dag en Tijd", en "Doelgroepen". Ook geef je bij iedere alinea welke bodaanpassing je gaat toepassen op basis van de verkgregen informatie. Doe dit op chronologische volgorde ook voor de andere campagne's. schrijf de alinea telkens als plain text ONDER de titel met een regel witruimte er tussenin, en gebruik geen markdown of tekens als ":". Wanneer je geen informatie over een alinea hebt weten te vinden, citeer je "Wij hebben geen gegevens kunnen vinden m.b.t. deze campagne en dit onderwerp".`;
-  */
-
-  const campaignPrompt: string = `Geef een analyze over de campagne genaamd ${campaignNames[campaignNamesCounter]}. Geef eerst de naam van de campagne (Campagne: <naam_campagne>), gevolgd door 5 alinea's: "Leeftijden", "Geslacht", "Apparaten", "Dag en Tijd, en "Doelgroepen". Schrijf de alinea telkens ONDER de titel met een regel witruimte er ussenin, en gebruik geen markdown of tekens als ":".`;
+  const detailedPrompt: string = `Geef een analyze over de volgende campagnes. Geef eerst de naam van de campagne (Campagne: <naam_campagne>), gevolgd door 5 alinea's: "Leeftijden", "Geslacht", "Apparaten", "Dag en Tijd, en "Doelgroepen". Schrijf de alinea telkens ONDER de titel met een regel witruimte er ussenin, en gebruik geen markdown of tekens als ":".`;
 
   const handleCompanyName = (event: any) => {
     setCompanyName(event.target.value);
@@ -145,7 +130,7 @@ function App() {
   };
 
   async function handleUpload() {
-    if (campaignRapport.length === 0) {
+    if (campaignData.length === 0) {
       return;
     }
 
@@ -170,37 +155,17 @@ function App() {
       };
 
       const analyzedCampagne = await combineTextAndAnalyze(
-        campaignRapport,
-        prompt
+        campaignData,
+        overviewPrompt
       );
 
       const analyzedCampaignOne = await combineTextAndAnalyze(
-        campaignOneDataFiles,
-        campaignPrompt
-      );
-
-      setCampaignNamesCounter(1);
-
-      const analyzedCampaignTwo = await combineTextAndAnalyze(
-        campaignTwoDataFiles,
-        campaignPrompt
-      );
-
-      setCampaignNamesCounter(2);
-
-      const analyzedCampaignThree = await combineTextAndAnalyze(
-        campaignThreeDataFiles,
-        campaignPrompt
+        campaignData,
+        detailedPrompt
       );
 
       setResponse(
-        analyzedCampagne.content +
-          "\n\n" +
-          analyzedCampaignOne.content +
-          "\n\n" +
-          analyzedCampaignTwo.content +
-          "\n\n" +
-          analyzedCampaignThree.content
+        analyzedCampagne.content + "\n\n" + analyzedCampaignOne.content
       );
       setIsUploading(false);
     } catch (err) {
@@ -223,74 +188,14 @@ function App() {
       <div className="dataForm">
         <label>Bedrijfsnaam</label>
         <input onChange={handleCompanyName}></input>
-        <label>CSV Kaartgegevens alle campagnes</label>
+        <label>Campagne data</label>
         <input
           className="fileInput"
           type="file"
           multiple
           onChange={(e) => {
             if (e.target.files) {
-              setCampaignRapport(Array.from(e.target.files));
-            }
-          }}
-        />
-        {/* Campagne 1*/}
-        <label>Campagnenaam</label>
-        <input
-          onChange={(e) => {
-            const updatedCampaignNames = [...campaignNames];
-            updatedCampaignNames[0] = e.target.value;
-            setCampaignNames(updatedCampaignNames);
-          }}
-        ></input>
-        <label>Kaartgegevens</label>
-        <input
-          className="fileInput"
-          type="file"
-          multiple
-          onChange={(e) => {
-            if (e.target.files) {
-              setCampaignOneDataFiles(Array.from(e.target.files));
-            }
-          }}
-        ></input>
-        {/* Campagne 2*/}
-        <label>Campagnenaam</label>
-        <input
-          onChange={(e) => {
-            const updatedCampaignNames = [...campaignNames];
-            updatedCampaignNames[1] = e.target.value;
-            setCampaignNames(updatedCampaignNames);
-          }}
-        ></input>
-        <label>Kaartgegevens</label>
-        <input
-          className="fileInput"
-          type="file"
-          multiple
-          onChange={(e) => {
-            if (e.target.files) {
-              setCampaignTwoDataFiles(Array.from(e.target.files));
-            }
-          }}
-        ></input>
-        {/* Campagne 3*/}
-        <label>Campagnenaam</label>
-        <input
-          onChange={(e) => {
-            const updatedCampaignNames = [...campaignNames];
-            updatedCampaignNames[2] = e.target.value;
-            setCampaignNames(updatedCampaignNames);
-          }}
-        ></input>
-        <label>Kaartgegevens</label>
-        <input
-          className="fileInput"
-          type="file"
-          multiple
-          onChange={(e) => {
-            if (e.target.files) {
-              setCampaignThreeDataFiles(Array.from(e.target.files));
+              setCampaignData(Array.from(e.target.files));
             }
           }}
         ></input>
@@ -298,7 +203,7 @@ function App() {
       <button onClick={handleUpload} disabled={isUploading}>
         {isUploading === true
           ? "Verwerken, even geduld..."
-          : "Laat ChatGPT analyzeren"}
+          : "Analyze uitvoeren"}
       </button>
       <small>versie: 1.0.0, model: OpenAI ChatGPT-4o-mini</small>
     </div>
